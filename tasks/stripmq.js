5c5817e0db257d9bc4f3fdf958e0aa08db007bac
J. Tangelder <j.tangelder@gmail.com>
2014-01-10 09:06:54 +0100

    code cleanup, fix try catch

--- tasks/stripmq.js
+++ tasks/stripmq.js
@@ -1,45 +1,40 @@
 'use strict';
 
 var fs = require("fs"),
-    stripmq = require('./lib/stripmq');
+  stripmq = require('./lib/stripmq');
 
 module.exports = function(grunt) {
-    grunt.registerMultiTask("stripmq", "Strip media queries from stylesheets", function () {
-        var options = this.options({});
+  grunt.registerMultiTask("stripmq", "Strip media queries from stylesheets", function() {
+    var options = this.options({});
 
-        // Iterate over all src-dest file pairs.
-        this.files.forEach(function(f) {
-            var src = f.src.filter(function (filepath) {
-                // Warn on and remove invalid source files (if nonull was set).
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
-                    return false;
-                } else {
-                    return true;
-                }
-            });
+    // Iterate over all src-dest file pairs.
+    this.files.forEach(function(f) {
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if(!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        }
+        return true;
+      });
 
+      // try and strip the mq
+      try {
+        var input = fs.readFileSync(src[0], {encoding: 'utf-8'});
+        var result = stripmq(input, options);
 
-            // Minify files, warn and fail on error.
-            var result;
-                var input = fs.readFileSync(src[0], {encoding:'utf-8'});
-                result = stripmq(input, options);
-            try {
-            } catch (e) {
-                var err = new Error('Stripping media queries failed.');
-                if (e.msg) {
-                    err.message += ', ' + e.msg + '.';
-                }
-                err.origError = e;
-                grunt.fail.warn(err);
-            }
-
-            // Write the destination file.
-            grunt.file.write(f.dest, result);
-
-            // Print a success message.
-            grunt.log.writeln('File "' + f.dest + '" created.');
-        });
+        grunt.file.write(f.dest, result);
+        grunt.log.writeln('File "' + f.dest + '" created.');
+      }
+      catch(e) {
+        var err = new Error('Stripping media queries failed.');
+        if(e.msg) {
+          err.message += ', ' + e.msg + '.';
+        }
+        err.origError = e;
+        grunt.fail.warn(err);
+      }
     });
+  });
 
 };

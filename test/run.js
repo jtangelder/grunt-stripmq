793fcd6e08522a0f23756beb8416a803a2835388
Jake Hickenlooper <jake@bottlerocket.net>
2014-06-04 08:34:20 -0600

    Depends on stripmq
    Moved the tasks/lib/stripmq.js to be in it's own repository.  This
    allows things like grunt, gulp, or a standalone cli to all use the same
    stripmq dependency.

--- test/run.js
+++ test/run.js
@@ -1,5 +1,5 @@
 var fs = require('fs'),
-    stripmq = require('../tasks/lib/stripmq');
+    stripmq = require('stripmq');
 
 var input = fs.readFileSync('input.css', {encoding:'utf-8'});
 var result = stripmq(input, {width: 1000, type: 'screen'});
